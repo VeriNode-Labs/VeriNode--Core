373dc847bbb838903391d10a9325b308931f5bf3
telemarkdigital-publisher <thetelemarkdigital@gmail.com>
2026-08-20 06:37:18 +0200

    chore: clean unused barrel exports

--- src/pool_manager/mod.rs
+++ src/pool_manager/mod.rs
@@ -1,5 +1,2 @@
 pub mod tenant_bond;
 pub mod reentrancy_guard;
-
-pub use tenant_bond::TenantBondManager;
-pub use reentrancy_guard::ReentrancyGuard;
--- src/reputation/mod.rs
+++ src/reputation/mod.rs
@@ -4,12 +4,5 @@ pub mod score;
 pub mod score_engine;
 pub mod types;
 
-pub use historical_window::{CircularWindow, WINDOW_SIZE};
-pub use score_engine::{
-    apply_decay, compute_weighted_average, decay_for_epochs, ema_update, reputation_weight,
-    update_reputation, MAX_DECAY_READOUT_ERROR,
-};
-pub use types::{
-    DecayFactor, EmaWeights, ReputationScore, TimeSinceLastUpdate, WindowSize, DEFAULT_DECAY_Q16,
-    DEFAULT_DECAY_Q32, MAX_REPUTATION,
-};
+pub use score_engine::{ema_update, update_reputation};
+pub use types::{DecayFactor, EmaWeights, MAX_REPUTATION};
